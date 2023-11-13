af7b5c7be245b1be982d80c079cd6897bdc9bcb1
IVO JONES <phuongsepfg@gmail.com>
2023-11-14 00:47:55 +0700

    .

--- c++.cpp
+++ c++.cpp
@@ -3088,9 +3088,9 @@ using namespace std;
 
     
 
-   int main(){
-    int n;
-    cin >> n;
+//    int main(){
+//     int n;
+//     cin >> n;
 
     /*
         *
@@ -3256,23 +3256,199 @@ using namespace std;
         ****
    */
 
-  for(int i =1 ;i<= n;i++)
-  {
-    for(int j =1 ;j<= n;j++)
-    {
-        if(j<=n-i){
+//   for(int i =1 ;i<= n;i++)
+//   {
+//     for(int j =1 ;j<= n;j++)
+//     {
+//         if(j<=n-i){
+//             cout << "~";
+//         }
+//         else{
+//             if(i==1||i==n||j==n-i+1||j==n)
+//             {
+//                 cout << "*";
+//             }else{
+//                 cout << " ";
+//             }
+//         }
+//     }
+//     cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    /*
+        *
+        **
+        ***
+        ****
+        ***
+        **
+        *
+    */
+
+    // for(int i = 1;i<=n ;i++){
+    //     for(int j =1 ;j <= i ;j++)
+    //     {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // for(int i = 1 ;i<=n-1 ;i++){
+    //     for(int j = 1 ; j <= n-i;j++){
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // return 0;
+
+    /*
+        ~~~~*
+        ~~~***
+        ~~*****
+        ~*******
+        *********
+        ~*******
+        ~~*****
+        ~~~***
+        ~~~~*
+    */
+
+//    for(int i = 1 ; i<= n ;i++){
+//         for(int j = 1 ;j <= n-i;j++)
+//         {
+//             cout << "~";
+//         }
+//         for(int j =1 ;j<=2*i-1;j++)
+//         {
+//             cout << "*";
+//         }
+//         cout<< endl;
+//    }
+//     for (int i = n-1 ;i>=1;--i){
+//         for (int j = 1 ;j<= n-i;j++){
+//             cout << "~";
+//         }
+//         for(int j = 1 ;j<=2*i-1;j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+
+    /*
+        ********
+        ***~~***
+        **~~~~**
+        *~~~~~~*
+        **~~~~**
+        ***~~***
+        ********
+    */
+
+//    for (int i = 1;i<=n ;i++)
+//    {
+//         for(int j = 1 ;j<=2*n;j++)
+//         {
+//             if(j<=n-i+1|| j>=n+i){
+//                 cout << "*";
+//             }else{
+//                 cout << "~";
+//             }
+//         }
+//         cout << endl;
+//    }
+//    for(int i = n -1 ;i>=1;--i){
+//         for(int j = 1;j<= 2*n;j++)
+//         {
+//             if(j<=n-i+1||j>=n+i){
+//                 cout << "*";
+//             }else{
+//                 cout << "~";
+//             }
+//         }
+//         cout <<endl;
+//    }
+
+
+    /*
+        *****
+        ~~****
+        ~~~~***
+        ~~~~~~**
+        ~~~~~~~~*
+        ~~~~~~**
+        ~~~~***
+        ~~****
+        *****
+    */
+
+//    for(int i =1 ;i<=n;i++)
+//    {
+//         for(int j = 1 ;j<=2*(i-1);j++)
+//         {
+//             cout << "~";
+//         }
+//         for(int j = 1 ;j<= n-i+1;j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//    }
+//    for(int i = n-1 ;i>=1;--i)
+//    {
+//         for(int j = 1 ;j<=2*(i-1);j++)
+//         {
+//             cout << "~";
+//         }
+//         for(int j = 1 ;j<= n-i+1;j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//    }
+
+    /*
+        ~~~~*****
+        ~~~****
+        ~~***
+        ~**
+        *
+        ~**
+        ~~***
+        ~~~****
+        ~~~~*****
+    */
+
+   for(int i =1 ;i<=n ;i++)
+   {
+        for (int j = 1 ;j<= n-i;j++)
+        {
             cout << "~";
         }
-        else{
-            if(i==1||i==n||j==n-i+1||j==n)
-            {
-                cout << "*";
-            }else{
-                cout << " ";
-            }
+        for(int j =1 ;j<= n-i+1;j++)
+        {
+            cout << "*";
         }
-    }
-    cout << endl;
-    }
-    return 0;
+        cout <<endl;
+   }
+   for(int i = n-1;i>=1;--i)
+   {
+        for (int j = 1 ;j<= n-i;j++)
+        {
+            cout << "~";
+        }
+        for(int j =1 ;j<= n-i+1;j++)
+        {
+            cout << "*";
+        }
+        cout <<endl; 
+   }
 }
