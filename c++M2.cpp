a2968f3236b933d8a909eb05fffd1a172cba17b5
IVO JONES <phuongsepfg@gmail.com>
2023-11-15 22:45:39 +0700

    .

--- c++M2.cpp
+++ c++M2.cpp
@@ -6,34 +6,38 @@ using namespace std;
 /// Cho mang A[i] gom n phan tu,
 /// hay sap xep lai cac pt cua mang
 /// sao cho A[i]=i. Neu pt la gia tri khac dua -1 vao
-// int main ()
-// {
-//     int tc;
-//     cin>> tc;
-//     while(tc--)
-//     {
-//         int n;
-//         cin >>n;
-//         map<long long, bool> mp;
-//         for(int i = 0 ;i<n;i++)
-//         {
-//             long long num;
-//             cin >> num;
-//             mp[num] = true;
-//         }
-//         for(int i = 0 ;i<n;i++){
-//             if(mp[i]){
-//                 cout << i << " ";
-//             }
-//             else{
-//                 cout << "-1 ";
-//             }
-//         }
-//         cout << endl;
-//     }
-//     return 0;
-// }
+int main ()
+{
+    int tc;
+    cin>> tc;
+    while(tc--)
+    {
+        int n;
+        cin >>n;
+        map<long long, bool> mp;
+        for(int i = 0 ;i<n;i++)
+        {
+            long long num;
+            cin >> num;
+            mp[num] = true;
+        }
+        for(int i = 0 ;i<n;i++){
+            if(mp[i]){
+                cout << i << " ";
+            }
+            else{
+                cout << "-1 ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
 
+
+/// cho mang A[] gom n so nguyen  bao gom so 0
+/// tim so nguyen duong nho nhat khong co mat trong mang
+/// VD: a[]={5,8,3,7,9,1} => 2
 int cnt[1000002];
 int main(){
     int tc; cin >> tc;
