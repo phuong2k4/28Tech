7849688bcb366c2eaf0f89abbbfc47c10ab5d2b3
IVO JONES <phuongsepfg@gmail.com>
2023-11-15 22:16:50 +0700

    .

--- c++M2.cpp
+++ c++M2.cpp
@@ -2,29 +2,36 @@
 
 using namespace std;
 
-int main ()
-{
-    int tc;
-    cin>> tc;
-    while(tc--)
-    {
-        int num;
-        cin >> num;
-        map<long long,bool> mp;
-        for(int i =0;i<num;i++){
-            long long n;
-            cin >> n;
-            mp[n] = true;
-        }
-        for(int i= 0;i<num;i++)
-        {
-            if(mp[i]){
-                cout << i << " ";
-            }else{
-                cout << "-1 " ;
-            }
-        }
-        cout << endl;
-    }
-    return 0;
-}
+
+/// Cho mang A[i] gom n phan tu,
+/// hay sap xep lai cac pt cua mang
+/// sao cho A[i]=i. Neu pt la gia tri khac dua -1 vao
+// int main ()
+// {
+//     int tc;
+//     cin>> tc;
+//     while(tc--)
+//     {
+//         int n;
+//         cin >>n;
+//         map<long long, bool> mp;
+//         for(int i = 0 ;i<n;i++)
+//         {
+//             long long num;
+//             cin >> num;
+//             mp[num] = true;
+//         }
+//         for(int i = 0 ;i<n;i++){
+//             if(mp[i]){
+//                 cout << i << " ";
+//             }
+//             else{
+//                 cout << "-1 ";
+//             }
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
