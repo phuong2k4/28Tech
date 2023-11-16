6d5dd2ebc592408c6969c5b9b07e1b53ea91d6da
IVO JONES <phuongsepfg@gmail.com>
2023-11-16 21:41:07 +0700

    .

--- c++M2.cpp
+++ c++M2.cpp
@@ -35,28 +35,28 @@ using namespace std;
 /// cho mang A[] gom n so nguyen  bao gom so 0
 /// tim so nguyen duong nho nhat khong co mat trong mang
 /// VD: a[]={5,8,3,7,9,1} => 2
-int cnt[1000002];
-int main(){
-    int tc;
-    cin >> tc;
-    while(tc--)
-    {
-        int n;
-        cin >> n;
-        memset(cnt,0,sizeof(cnt));
-        for(int i =0 ;i<n;i++){
-            int x;
-            cin >> x;
-            if(x>0)cnt[x]=1;
-        }
-        for(int i = 1;i<=1000001 ;i++){
-            if(cnt[i]==0){
-                cout << i << " ";
-                break;
-            }
-        }
-        cout << endl;
-    }
-    return 0;
-}
+// int cnt[1000002];
+// int main(){
+//     int tc;
+//     cin >> tc;
+//     while(tc--)
+//     {
+//         int n;
+//         cin >> n;
+//         memset(cnt,0,sizeof(cnt));
+//         for(int i =0 ;i<n;i++){
+//             int x;
+//             cin >> x;
+//             if(x>0)cnt[x]=1;
+//         }
+//         for(int i = 1;i<=1000001 ;i++){
+//             if(cnt[i]==0){
+//                 cout << i << " ";
+//                 break;
+//             }
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
 
