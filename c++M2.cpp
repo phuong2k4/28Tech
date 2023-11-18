5dffe559cc757dcc559937508b8ae07fe515ea69
IVO JONES <phuongsepfg@gmail.com>
2023-11-18 14:05:54 +0700

    .

--- c++M2.cpp
+++ c++M2.cpp
@@ -89,24 +89,24 @@ using namespace std;
 //     return 0;
 // }
 
-int main (){
-    int tc;
-    cin >> tc;
-    while(tc--){
-        int n;
-        cin >> n;
-        int a[n];
-        for(int i =0;i<n;i++)
-        {
-            cin >> a[i];
-        }
-        sort(a,a + n);
-        int res = INT_MAX;
-        for(int i =1 ;i<n;i++)
-        {
-            res = min(res , a[i]-a[i-1]);
-        }
-        cout << res << endl;
-    }
-    return 0;
-}
+// int main (){
+//     int tc;
+//     cin >> tc;
+//     while(tc--){
+//         int n;
+//         cin >> n;
+//         int a[n];
+//         for(int i =0;i<n;i++)
+//         {
+//             cin >> a[i];
+//         }
+//         sort(a,a + n);
+//         int res = INT_MAX;
+//         for(int i =1 ;i<n;i++)
+//         {
+//             res = min(res , a[i]-a[i-1]);
+//         }
+//         cout << res << endl;
+//     }
+//     return 0;
+// }
